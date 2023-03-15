47930dadfcbb02c0d8d7c7785ab40f451343c1ca
ayushmcodes <ayushmalik24@gmail.com>
2023-03-15 20:40:18 +0530

    making search app using react

---
+++ searchapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import './styles.css'
+
+
+
+function App() {
+
+
+  return (
+    <React.Fragment>
+    <div id="card">
+    <div id="inputholder"><input id="input" type="text"/></div>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
