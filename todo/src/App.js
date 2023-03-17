d66a064a979327db3a147126cbcf643fd629cdb5
ayushmcodes <ayushmalik24@gmail.com>
2023-03-17 18:55:21 +0530

    working on todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './styles.css';
+import {AiFillPlusSquare} from "react-icons/ai";
 
 function App() {
-  return (
-    <React.Fragment>
-    <h1>ToDo made using React</h1>
-    <div id="div1">
-    <input type="text" id="input1"/> 
-    <input type="text" id="input2"/>
-    </div>
-    <div>
-    </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <h1>ToDo made using React</h1>
+            <div id="div1">
+                <input type="text" id="input1"/>
+                <AiFillPlusSquare id="addicon"/>
+                <select id="input2">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="left">Left</option>
+                </select>
+            </div>
+            <div></div>
+        </React.Fragment>
+    );
 }
 
 export default App;
