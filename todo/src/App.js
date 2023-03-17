a7e0c46905be0e0efe398e72ad9108396f8a8149
ayushmcodes <ayushmalik24@gmail.com>
2023-03-17 11:51:26 +0530

    working on react todo app

---
+++ todo/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <React.Fragment>
+    <h1>ToDo made using React</h1>
+    <div id="div1">
+    <input type="text" id="input1"/> 
+    <input type="text" id="input2"/>
+    </div>
+    <div>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
