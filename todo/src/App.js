04849cbfd43aea1b65db9382574c34053f80b8c4
ayushmcodes <ayushmalik24@gmail.com>
2023-03-19 22:38:03 +0530

    working on todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,20 +4,34 @@ import './App.css';
 import './styles.css';
 import {AiFillPlusSquare} from "react-icons/ai";
 
+
+
 function App() {
+
+    
+    function extractinput(){
+        let text="";
+        text=document.getElementById("input1").value;
+        console.log(text);
+    }
+
     return (
         <React.Fragment>
             <h1>ToDo made using React</h1>
             <div id="div1">
                 <input type="text" id="input1"/>
-                <AiFillPlusSquare id="addicon"/>
+                <AiFillPlusSquare id="addicon" onClick={extractinput} />
                 <select id="input2">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="left">Left</option>
+                    <div>
+                    </div>
+                    
                 </select>
             </div>
-            <div></div>
+
+            <div id="cardholder"></div>
         </React.Fragment>
     );
 }
